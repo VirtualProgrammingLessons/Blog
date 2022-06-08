cec858734a2d84958a183b47b3823c20f966d71f
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-06-08 18:03:09 -0400

    update

---
+++ client/src/Pages/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Helmet} from "react-helmet";
+
+const Blog = () => {
+  return (
+    <div>
+      
+
+    </div>
+  )
+}
+
+export default Blog
---
+++ client/src/Pages/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Post = () => {
+  return (
+    <div>
+        <h1>Title</h1>
+        <div>
+            Post
+        </div>
+    </div>
+  )
+}
+
+export default Post
